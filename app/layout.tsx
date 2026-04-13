98c166d35499f6a3e4b86c2f990418209e4a5e59
htk2506 <htk2506@gmail.com>
2026-04-12 22:47:49 -0500

    Adjust theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,8 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v16-appRouter';
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import { ThemeProvider } from '@mui/material/styles';
+import theme from '../theme';
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,11 +16,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`antialiased`}>
         <AppRouterCacheProvider options={{ enableCssLayer: true }}>
-          {children}
+          <ThemeProvider theme={theme}>
+            {children}
+          </ThemeProvider>
         </AppRouterCacheProvider>
       </body>
     </html>
