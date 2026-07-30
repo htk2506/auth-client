a0d0d17276a9f11795cf50a1fca5c577bdfa799d
htk2506 <htk2506@gmail.com>
2026-07-29 22:37:59 -0500

    WIP: Settings page

---
+++ app/settings/page.tsx
@@ -0,0 +1,146 @@
+
+'use client'
+import { useGetCurrentUserQuery } from '@/lib/api-slice';
+import { Alert, Box, Button, CircularProgress, Link, TextField, Typography } from '@mui/material';
+import { SerializedError } from '@reduxjs/toolkit';
+import { useFormik } from 'formik';
+import { useState } from 'react';
+import * as yup from 'yup';
+
+const validationSchema = yup.object({
+    username: yup
+        .string()
+        .required('Username is required.'),
+    note: yup
+        .string(),
+    email: yup
+        .string()
+        .email(),
+});
+
+export default function SettingsPage() {
+    const [isEditMode, setIsEditMode] = useState<boolean>(false);
+    const [updateUserErrorMessage, setUpdateUserErrorMessage] = useState<string>('');
+    const {
+        data: currentUser,
+        isLoading: getCurrentUserIsLoading,
+        isSuccess: getCurrentUserIsSuccess,
+        isError: getCurrentUserIsError,
+        error: getCurrentUserError,
+    } = useGetCurrentUserQuery();
+
+    // Toggles whether or not to show password plain text
+    const handleClickSetEditMode = () => setIsEditMode((isEditMode) => !isEditMode);
+
+    const formik = useFormik({
+        initialValues: {
+            username: currentUser?.username
+        },
+        validationSchema: validationSchema,
+        onSubmit: async (values) => {
+            try {
+                // TODO: Trigger call to update
+
+            } catch (err: any) {
+                setUpdateUserErrorMessage(err.data.detail);
+                console.error(`Failed to  update: ${JSON.stringify(err)}`);
+            }
+        },
+    });
+
+    if (getCurrentUserIsLoading) { // Loading placeholder
+
+        <Box className='flex flex-col items-center'>
+            <CircularProgress aria-label='Loading…' color='inherit' size='20px' />
+        </Box>
+    }
+    else { // Once info has loaded
+        if (getCurrentUserIsError) { // If there was an error
+
+            // Display an error
+            return (
+                <Box className='flex flex-col items-center'>
+
+                    <Alert variant='outlined' severity='error' className='mb-5'>
+                        {(getCurrentUserError as SerializedError)?.message || 'Something went wrong.'}
+                    </Alert>
+
+                    <Link href='/login'>
+                        Login
+                    </Link>
+
+                </Box>
+            );
+        } else {
+
+            // Show the form
+            return (
+                <Box className='flex flex-col items-center'>
+                    <Box className='w-95/100 p-5 rounded-lg' sx={{ boxShadow: 1 }}>
+                        <form onSubmit={formik.handleSubmit}>
+                            <Box className='flex flex-col gap-2'>
+
+                                <Typography variant='h1' className='text-2xl mb-2'>
+                                    Profile
+                                </Typography>
+
+                                <TextField
+                                    fullWidth
+                                    disabled
+                                    id='user-id'
+                                    name='user-id'
+                                    label='User ID'
+                                    value={currentUser?.id}
+                                    slotProps={{
+                                        input: {
+                                            readOnly: true,
+                                        },
+                                    }}
+                                    autoComplete='off'
+                                    data-1p-ignore data-lpignore="true" data-protonpass-ignore="true"
+                                    helperText={' '}
+                                />
+
+                                <TextField
+                                    fullWidth
+                                    id='username'
+                                    name='username'
+                                    label='Username'
+
+                                    defaultValue={currentUser?.username}
+                                    value={formik.values.username}
+                                    onChange={formik.handleChange}
+                                    onBlur={formik.handleBlur}
+                                    error={formik.touched.username && Boolean(formik.errors.username)}
+                                    helperText={formik.touched.username && formik.errors.username || ' '}
+                                    
+                                    autoComplete='off'
+                                />
+
+
+                                {/* {isLoading &&
+                            <CircularProgress aria-label='Loading…' className='mx-auto mb-5' />
+                        }
+
+                        {isError &&
+                            <Alert variant='outlined' severity='error' className='mb-5'>
+                                {loginErrorMessage || 'Something went wrong.'}
+                            </Alert>
+                        } */}
+
+                                <Button
+                                    fullWidth
+                                    color='primary'
+                                    variant='contained'
+                                    type='submit'
+                                >
+                                    Edit
+                                </Button>
+                            </Box>
+                        </form>
+                    </Box>
+                </Box>
+            );
+        }
+    }
+}
