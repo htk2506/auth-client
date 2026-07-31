8f98b0a1827cac3ea417847cb03423607cc0d171
htk2506 <htk2506@gmail.com>
2026-07-30 21:34:19 -0500

    Adjust login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -64,7 +64,7 @@ export function LoginForm({ }: Readonly<{}>) {
 
   return (
     <Box className='flex flex-col items-center'>
-      <Box className='w-9/10 sm:w-sm p-5 rounded-lg' sx={{ boxShadow: 1 }}>
+      <Box className='w-95/100 sm:w-sm p-5 rounded-lg' sx={{ boxShadow: 1 }}>
         <form onSubmit={formik.handleSubmit}>
           <Box className='flex flex-col gap-2'>
 
